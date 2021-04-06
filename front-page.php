cc1439804b788c47d8736fdc3c8300f229e29179
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-07 02:26:35 +0900

    Add intro section

--- front-page.php
+++ front-page.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
 
+<section class="intro">
+  <div class="section__inner">
+    <h2>TOEFL学習でこんな悩みありませんか？</h2>
+    <ul>
+      <li>勉強の習慣が<br>身についていない</li>
+      <li>勉強しているはず<br>なのに点数が伸びない</li>
+      <li>正しい勉強方法が<br>わからない</li>
+    </ul>
+    <div class="intro__box">
+      <h2>Engressは<br><span>TOEFLに特化したスクール</span>です</h2>
+      <p>完全オーダーメイドで、１人１人の悩みに合わせた最適な指導で<br>TOEFLの苦手分野を克服します。</p>
+    </div>
+  </div><!-- /.section__inner -->
+</section><!-- /.intro -->
+
 <?php get_footer(); ?>
