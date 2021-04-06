1b5905efd9203682d7a5aa96c5d60ebd50df0e89
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-06 14:11:43 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title><?php bloginfo('name'); ?></title>
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>" />
+
+<!-- RESET CSS -->
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/ress/dist/ress.min.css">
+
+  <?php wp_head(); ?>
