83e2d44c9c219f3cf108fafd041575c16f7290b9
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-07 11:16:56 +0900

    Add features section

--- front-page.php
+++ front-page.php
@@ -15,4 +15,46 @@
   </div><!-- /.section__inner -->
 </section><!-- /.intro -->
 
+<section class="features">
+  <div class="section__inner">
+    <h2 class="section__title">TOEFL対策に特化したEngress3つの強み</h2>
+    <ul class="features__list">
+      <li class="features__item">
+        <div>
+          <p class="features__number">特長 １</p>
+          <h3>TOEFLに最適化された<br>無駄のないカリキュラム</h3>
+          <p>
+            TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。
+          </p>
+        </div>
+        <figure><img src="<?php echo get_template_directory_uri(); ?>/images/feature01.png" alt=""></figure>
+      </li><!-- /.features__item -->
+      <li class="features__item">
+        <div>
+          <p class="features__number">特長 ２</p>
+          <h3>日本人指導歴10年以上の<br>経験豊富な講師陣</h3>
+          <p>
+            Engressの講師陣は、もともと日本人向けにTOEFLを教えていた人が大多数です。また全メンバーがTESOL(英語教授法)を取得しており、知識と経験を兼ね備えている教育のプロフェッショナルです。
+          </p>
+        </div>
+        <figure><img src="<?php echo get_template_directory_uri(); ?>/images/feature02.png" alt=""></figure>
+      </li><!-- /.features__item -->
+      <li class="features__item">
+        <div>
+          <p class="features__number">特長 ３</p>
+          <h3>平均3ヶ月でTOEFL iBT20点アップ</h3>
+          <p>
+            Engressは高校生からサラリーマンまで様々な年齢層の方々が通われていますが、完全オーダーメイドのカリキュラムで柔軟に対応しているため、平均3ヶ月でTOEFLスコアを20点アップさせています。
+          </p>
+        </div>
+        <figure><img src="<?php echo get_template_directory_uri(); ?>/images/feature03.png" alt=""></figure>
+      </li><!-- /.features__item -->
+    </ul><!-- /.features__list -->
+    <div class="features__price">
+      <p>Engressの料金プランはこちら</p>
+      <a class="features__btn" href="">料金を見てみる</a>
+    </div>
+  </div><!-- /.section__inner -->
+</section>
+
 <?php get_footer(); ?>
