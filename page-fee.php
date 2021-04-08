407a00949ab4a4cb775316ca9c44bed19819881f
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-08 14:25:54 +0900

    Create page-fee.php & Add STRUCTURE section

---
+++ page-fee.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<section class="structure">
+  <div class="section__inner">
+    <h2 class="section__title">料金体系</h2>
+    <div class="structure__formula">
+      <div>入会金 39,800円</div><span>+</span><div>月額費用</div>
+    </div>
+    <p>
+      Engressのカリキュラムは完全オーダーメイドのため、カリキュラム内のサポート内容によって料金が変動します。おすすめのスタンダードプランでは、進学先に合わせたサポートまで包括的に行います。
+    </p>
+  </div><!-- /.section__inner -->
+</section><!-- /.features -->
+
+<?php get_footer(); ?>
