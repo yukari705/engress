9536b1538e122ee5ef34e182978b0c21f516a4c1
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-09 12:00:20 +0900

    Add sidebar & custom menu bar function

--- functions.php
+++ functions.php
@@ -14,3 +14,24 @@ add_theme_support('post-thumbnails');
 
     // ヘッダー用のサイズ設定
     add_image_size('masthead', 2560, 440, true);
+
+
+// ヘッダー、フッターのカスタムメニュー化
+register_nav_menus(
+    array(
+        'place_global' => 'Global',
+        'place_footer' => 'Footer-nav',
+    )
+);
+
+// ウィジェット機能を有効化
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+    'name' => 'サイドバー',
+    'id' => 'sidebar',
+    'before_widget' => '<aside>',
+    'after_widget' => '</aside>',
+    'before_title' => '<h2 class="sidebar__title">',
+    'after_title' => '</h2>'
+    ));
+}
