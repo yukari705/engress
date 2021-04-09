569b70cdfcc350aee674681e9838cb56c54ea8de
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-10 00:07:40 +0900

    Create pagenation.php

--- page-blog.php
+++ page-blog.php
@@ -3,21 +3,10 @@
 <section class="blog-archive">
     <div class="section__inner">
         <h2 class="section__title">新着一覧</h2>
-            <?php get_template_part('content-archive-blog'); ?>
+        <?php get_template_part('content-archive-blog'); ?>
     </div><!-- /.section__inner -->
 </section><!-- /.blog-archive -->
 <aside class="pagenation">
-    <?php
-    if ($the_query->max_num_pages > 1) :
-        echo paginate_links(array(
-            'base' => get_pagenum_link(1) . '%_%',
-            'format' => 'page/%#%/',
-            'current' => max(1, $paged),
-            'total' => $the_query->max_num_pages,
-            'prev_next' => false
-        ));
-    endif;
-    ?>
+    <?php get_template_part('pagenation'); ?>
 </aside>
-<?php wp_reset_postdata(); ?>
 <?php get_footer(); ?>
