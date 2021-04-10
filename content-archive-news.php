f131cc4c043d47e69bd5ba4f9e371913be4d3f58
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-11 00:18:12 +0900

    Create content-archive-news.php

---
+++ content-archive-news.php
@@ -0,0 +1,16 @@
+<ul class="news-archive__list">
+    <?php
+    $the_query = get_all_posts('news');
+    if ($the_query->have_posts()) :
+        while ($the_query->have_posts()) : $the_query->the_post();
+    ?>
+            <li class="news-archive__item">
+                <p class="news-archive__date"><?php the_time('Y-m-d'); ?></p>
+                <a href="<?php the_permalink(); ?>">
+                    <h1 class="news-archive__title"><?php the_title(); ?></h1>
+                </a>
+            </li>
+    <?php endwhile;
+        wp_reset_postdata();
+    endif; ?>
+</ul>
