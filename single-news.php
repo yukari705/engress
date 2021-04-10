aa82e360f891559ae3949f74442172346642cfd6
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-10 14:55:04 +0900

    Add POST NEWS section

--- single-news.php
+++ single-news.php
@@ -1,2 +1,21 @@
 <?php get_header(); ?>
+
+<div class="post--news">
+    <div class="section__inner">
+        <article>
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
+                    <h1 class="post__title"><?php the_title(); ?></h1>
+                    <p class="post__date"><?php the_time('Y-m-d'); ?></p>
+                    <div class="post__content"><?php the_content(); ?></div>
+            <?php
+                endwhile;
+            endif;
+            ?>
+        </article>
+    </div><!-- /.section__inner -->
+</div><!-- /.post--news -->
+
+
 <?php get_footer(); ?>
