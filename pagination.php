8e65274999a05b308fca38973f9ffe44c5ada1ee
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-11 14:56:58 +0900

    Change pagination

--- pagination.php
+++ pagination.php
@@ -1,17 +1,23 @@
 <aside class="pagination">
     <?php
-    if (is_page('blog')) {
+    $big = 999999999; // need an unlikely integer
+
+    if (is_page('blog')) :
         $the_query = get_all_posts('post');
-    } elseif (is_post_type_archive('news')) {
-        $the_query = get_all_posts('news');
-    }
-    if ($the_query->max_num_pages > 1) {
         echo paginate_links(array(
             'base' => get_pagenum_link(1) . '%_%',
             'format' => 'page/%#%/',
             'current' => max(1, $paged),
-            'total' => $the_query->max_num_pages,
-            'prev_next' => false
+            'prev_next' => false,
+            'total' => $the_query->max_num_pages
         ));
-    }; ?>
+    else :
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => 'page/%#%/',
+            'current' => max(1, $paged),
+            'prev_next' => false,
+        ));
+    endif;
+    ?>
 </aside>
