3dbc2b85e82057194ed2d97304004a2203c9c0b5
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-13 02:36:05 +0900

    Add setting of triming title

--- content-archive-news.php
+++ content-archive-news.php
@@ -7,7 +7,7 @@
             <li class="news-archive__item">
                 <p class="news-archive__date"><?php the_time('Y-m-d'); ?></p>
                 <a href="<?php the_permalink(); ?>">
-                    <h1 class="news-archive__title"><?php the_title(); ?></h1>
+                    <h1 class="news-archive__title"><?php echo wp_trim_words( get_the_title(), 50, '...' ); ?></h1>
                 </a>
             </li>
     <?php endwhile;
