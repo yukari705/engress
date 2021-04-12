5ea53ae91c200e60b9a6349493b15e81a9cd9619
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-13 02:36:54 +0900

    Add comment to contact

--- page-contact.php
+++ page-contact.php
@@ -2,6 +2,7 @@
 
 <section class="contact">
     <div class="section__inner">
+        弊社に興味を持って頂きありがとうございます。商談やサービスのご利用、資料請求について下記のフォームよりお問い合わせください。
         <div class="contact-form">
             <?php if (have_posts()) :
                 while (have_posts()) : the_post();
