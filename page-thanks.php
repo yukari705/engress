e5eb1172397f503cc4dc23b9642fcf35f2386744
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-12 18:45:28 +0900

    Create page-thanks.php

---
+++ page-thanks.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="thanks">
+    <div class="section__inner">
+        <div class="thanks__message">
+            <p>お問い合わせいただきありがとうございます。<br>
+            内容を確認した後、担当者よりご連絡いたします。</p>
+            <a href="/">ホームへ戻る</a>
+        </div><!-- /.contact-form -->
+    </div><!-- /.section__inner -->
+</section><!-- /.contact -->
+
+<?php get_footer(); ?>
