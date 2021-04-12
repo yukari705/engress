40c1bb28673fae17d84bb1293d157c168c6085a3
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-12 18:16:18 +0900

    Add CONTACT section

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,16 @@
 <?php get_header(); ?>
 
+<section class="contact">
+    <div class="section__inner">
+        <div class="contact-form">
+            <?php if (have_posts()) :
+                while (have_posts()) : the_post();
+                    the_content();
+                endwhile;
+            endif;
+            ?>
+        </div><!-- /.contact-form -->
+    </div><!-- /.section__inner -->
+</section><!-- /.contact -->
 
 <?php get_footer(); ?>
