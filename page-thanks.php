7c8fb02874b4f8ee67cf640ad49e3f8cc51e5971
yukari705 <80951042+yukari705@users.noreply.github.com>
2021-04-15 00:51:12 +0900

    Change link url

--- page-thanks.php
+++ page-thanks.php
@@ -5,7 +5,7 @@
         <div class="thanks__message">
             <p>お問い合わせいただきありがとうございます。<br>
             内容を確認した後、担当者よりご連絡いたします。</p>
-            <a href="/">ホームへ戻る</a>
+            <a href="<?php echo esc_url(home_url()); ?>">ホームへ戻る</a>
         </div><!-- /.contact-form -->
     </div><!-- /.section__inner -->
 </section><!-- /.contact -->
